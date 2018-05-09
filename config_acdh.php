f9f7b959eea2ddb613e59f300d93135822d244a2
Asil Çetin <25176039+asilcetin@users.noreply.github.com>
2018-05-09 11:55:45 +0200

    config acdh for tracking added

---
+++ config_acdh.php
@@ -0,0 +1,16 @@
+<?php
+
+/* This file defines properties of ACDH services to connect */
+define("ACDH_DEPLOYMENT", "DEV");
+
+if (ACDH_DEV == "DEV") { /* DEFINE DEVELOPMENT SERVICES */
+
+} else if (ACDH_DEV == "TEST") { /* DEFINE TEST SERVICES */
+
+} else if (ACDH_DEV == "STAGING") { /* DEFINE STAGING SERVICES */
+
+} else if (ACDH_DEV == "PROD") { /* DEFINE PRODUCTION SERVICES */
+
+}
+
+?>
